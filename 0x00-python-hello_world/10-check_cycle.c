9e71f18a76c979677941e4bf7a1bd966f60fe2fc
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-08-10 17:51:23 +0100

    hot fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,13 +8,14 @@
 #include "list.h"
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list->next;
+	listint_t *slow, *fast;
 
 	if (list == NULL || list->next == NULL)
 	{
 		return (0); /* No cycle if the list is empty or has only one element */
 	}
+	slow = list;
+	fast = list->next;
 	while (fast != NULL && fast->next != NULL)
 	{
 		if (slow == fast)
