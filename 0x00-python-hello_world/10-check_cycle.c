b5b97148922de1577d5aebcd862d0d8787006a1e
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-08-10 17:38:37 +0100

    add file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+/**
+ * check_cycle - a function that checks if a
+ * singly linked list has a cycle in it.
+ *
+ * @list: struct array
+ * Return: 0 if there is no cycle else 1.
+ */
+#include "list.h"
+int check_cycle(listint_t *list)
+{
+	if (list == NULL || list->next == NULL)
+	{
+		return (0); /* No cycle if the list is empty or has only one element */
+	}
+	listint_t *slow = list;
+	listint_t *fast = list->next;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+		{
+			return (1); /* Cycle detected */
+		}
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
