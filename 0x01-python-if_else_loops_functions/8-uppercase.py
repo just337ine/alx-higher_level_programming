ee86377e9ec349d95c3d12d6514e6b95eafff864
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-08-17 15:26:29 +0100

    solve task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            upper_char = chr(ord(c) -32)
+        else:
+            upper_char = c
+        print("{:s}".format(upper_char), end="")
+    print()
