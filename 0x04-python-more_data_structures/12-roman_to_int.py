7a30a88778830b15450ca65692ee9ff3def492d8
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-09-03 04:34:48 +0100

    solve task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    result = 0
+    i = 0
+
+    while i < len(roman_string):
+        current_value = roman_dict.get(roman_string[i], 0)
+        next_value = roman_dict.get(roman_string[i + 1], 0) if i + 1 < len(roman_string) else 0
+
+        if current_value < next_value:
+            result += next_value - current_value
+            i += 2
+        else:
+            result += current_value
+            i += 1
+
+    return result
