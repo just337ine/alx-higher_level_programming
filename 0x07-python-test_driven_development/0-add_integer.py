9bf8fb8f8e779b7d91ef6c792170bd144e264942
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-09-08 21:28:38 +0100

    create add function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+A Simple addition module
+'''
+
+def add_integer(a, b=98):
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
+    result = int(a) + int(b)
+    return result
+
+if __name__ == '__main__':
+    from doctest import testfile
+    testfile('test/0-add_integer.txt')
