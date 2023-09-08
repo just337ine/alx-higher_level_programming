6cea45a4a282bc2b022524f50e1c3da9b7e5c59d
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-09-08 21:44:52 +0100

    fix test case

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@ A Simple addition module
 
 
 def add_integer(a, b=98):
+    ''' a function that add two integers or floats. '''
     if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
     if type(b) is not int and type(b) is not float:
