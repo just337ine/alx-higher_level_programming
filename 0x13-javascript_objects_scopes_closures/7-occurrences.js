5d61a7d0ceacf3d2b15a138fef9f8596fe7ff752
Ugwu Justine <ugwujustineobinna@gmaiil.com>
2023-11-15 01:07:37 +0100

    solve task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let index = 0; index < list.lenght; index++) {
+  for (let index = 0; index < list.length; index++) {
     if (list[index] === searchElement) {
       count++;
     }
