db8b9b3d272a4779812cb1c807bf2429a733c1ac
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-11-27 00:31:05 +0100

    solve task 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class LockedClass"""
+
+class LockedClass:
+    """
+    Function that prevents the user from dynamically
+    creating new instance attributes...
+    """
+
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """Creates new instance of Licked Class."""
+
+        self.first_name = "first_name"
