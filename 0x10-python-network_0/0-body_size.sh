acb2c8a2ea67e26606700ccd25ea8dbf2bad5f0c
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-12-06 05:52:59 +0100

    Update file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script that displauys the size of the body of the url
-response_size=$(curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r\n')
+curl -s "$1" | wc -c
