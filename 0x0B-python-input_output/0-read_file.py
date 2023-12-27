a7b73795270e00f343e14f7b72d0d13bca354afd
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-12-27 09:40:38 +0100

    update file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """A function that reads a text file (UTF8)
+    and prints it to stdout
+    """
     with open(filename, 'r') as f:
         for line in f:
             print(line, end="")
