7cd5e134a6576341f4060325e2162d4011b1bddf
Ugwu Justine <ugwujustineobinna@gmail.com>
2023-12-27 10:25:30 +0100

    task 3 solution

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" A function that returns the Json repr of an object"""
+from json import dumps as dp
+
+def to_json_string(my_obj):
+    """A function that returns the Json repr of an object"""
+    return dp(my_obj)
