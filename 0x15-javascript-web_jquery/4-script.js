35413d22ceebdaa6581606acbedc153dd1a9f7ee
Just Ine <ugwujustineobinna@gmail.com>
2024-01-17 05:23:48 +0100

    solve task 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$(function() {
+	$("#toggle_header").click(function() {
+		if($("header").hasClass("green")) {
+			$("header").removeClass("green")
+			$("header").addClass("red")
+		} else {
+			$("header").addClass("green")
+		}
+	});
+});
